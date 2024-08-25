033fe6d94db41e52fffe8469d932ef7680be76c5
omodtung <dothedung131@gmail.com>
2024-08-25 23:53:24 +0700

    Home Site

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,94 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { Metadata } from "next";
+import { getAllBlog } from "@/services/BlogService";
+import { blogType } from "@/types";
+import Link from "next/link";
+import moment from "moment";
+export const metadata: Metadata = {
+  title: "HomePageBlog",
+  description: "HomePage Blog",
+};
 
-export default function Home() {
+export default async function Home() {
+  const latestBlogs: blogType[] = await getAllBlog({ items_per_page: "3" });
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div id="home" className="intro route bg-image">
+        <div className="overlay-itro"></div>
+        <div className="intro-content display-table">
+          <div className="table-cell">
+            <div className="container">
+              <h1 className="intro-title mb-4">I am a SE</h1>
+              <p className="intro-subtitle">
+                <span className="text-slider-items">
+                  I always code in JavaScript, when I do, it's fullstack.
+                </span>
+                <strong className="text-slider"></strong>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <section id="blog" className="blog-mf sect-pt4 route">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="title-box text-center">
+                <h3 className="title-a">Latest Blog</h3>
+                <div className="line-mf"></div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            {latestBlogs.map((item) => {
+              return (
+                <div className="col-md-4">
+                  <div className="card card-blog">
+                    <div className="card-img">
+                      <Link href={`/blog/${item.id}`}>
+                        <img
+                          src={`${process.env.API_URL}/${item.thumbnail}`}
+                          alt={item.title}
+                          className="img-fluid"
+                        />
+                      </Link>
+                    </div>
+                    <div className="card-body">
+                      <div className="card-category-box">
+                        <div className="card-category">
+                          <h6 className="category">{item.category.name}</h6>
+                        </div>
+                      </div>
+                      <h3 className="card-title">
+                        <Link href={`/blog/${item.id}`}>{item.title}</Link>
+                      </h3>
+                      <p className="card-description">
+                        {item.summary.length > 120
+                          ? `${item.summary.slice(0, 120)}...`
+                          : item.summary}
+                      </p>
+                    </div>
+                    <div className="card-footer">
+                      <div className="post-author">
+                        <img
+                          src={`${process.env.API_URL}/${item.user.avatar}`}
+                          alt=""
+                          className="avatar rounded-circle"
+                        />
+                        <span className="author">{item.user.first_name}</span>
+                      </div>
+                      <div className="post-date">
+                        <span className="ion-ios-clock-outline"></span>{" "}
+                        <i className="fa fa-calendar" aria-hidden="true"></i>{" "}
+                        {moment(item.created_at).format("DD/MM/YYY")}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
